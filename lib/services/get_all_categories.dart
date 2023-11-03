a4320a2c75e338e42395ec4e7118aa82d5dd3d35
Administrator <hemo089@outlook.com>
2023-11-03 17:16:38 +0200

    services

---
+++ lib/services/get_all_categories.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+class GetAllCategories
+{
+  Future<List<dynamic>> getCategories()
+  async{
+    http.Response response = await http.get(Uri.parse("https://fakestoreapi.com/products/categories"));
+    List<dynamic> data = jsonDecode(response.body);
+    List<dynamic> categoriesList = [];
+    for(int i = 0; i < data.length ; i++){
+      categoriesList.add(
+        data[i],
+      );
+    }
+    return categoriesList;
+  }
+
+}
